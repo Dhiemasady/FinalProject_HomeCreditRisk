c3efea9840cdcd5801172a315927db4a164439a5
Dhiemas Ady Kusuma W <81785278+Dhiemasady@users.noreply.github.com>
2022-12-12 17:57:38 +0700

    Update

--- application.py
+++ application.py
@@ -75,7 +75,7 @@ def main():
         NAME_INCOME_TYPE=7
 
     NAME_CONTRACT_TYPE = st.selectbox(
-        'Clien\’s loan annuity',
+        'Clien\’s type of loans',
         ('Cash Loans','Revolving Loans'))
     'You selected:', NAME_CONTRACT_TYPE
     if NAME_CONTRACT_TYPE=="Cash Loans":
@@ -91,7 +91,7 @@ def main():
 
     DAYS_BIRTH = st.text_input('Client\'s age this year')
 
-    AMT_ANNUITY = st.text_input('Amount of client\'s income in a year')
+    AMT_ANNUITY = st.text_input('Clien\’s loan annuity')
 
     AMT_GOODS_PRICE = st.text_input('Amount of client want to loan')
 
